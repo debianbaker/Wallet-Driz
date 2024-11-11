a996347775395666650caa69e3f993670ab7bc85
debianbaker <linuxrocker002@gmail.com>
2024-11-11 23:15:37 +0530

    modified config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import wasm from 'vite-plugin-wasm'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), wasm()],
+  build: {
+    target: 'esnext'  // Use the latest JavaScript features, including top-level await
+  }
 })
